f946a5a3585fda9af62ded3aa10e56c6f467f357
S1S4 <Cesar_Zhiro26@hotmail.com>
2023-08-31 15:35:17 -0400

    added styles to navbar--mobile-menu elements

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const navbarIcon = document.querySelector('.navbar-icon');
-const navProduct = document.querySelector('.navbar-options--product');
-const navCompany = document.querySelector('.navbar-options--company');
-const navConnect = document.querySelector('.navbar-options--connect');
-const navSubProduct = document.querySelector('.subOptions-product');
-const navSubCompany = document.querySelector('.subOptions-company');
-const navSubConnect = document.querySelector('.subOptions-connect');
+const navProduct = document.querySelector('.desktop-options--product');
+const navCompany = document.querySelector('.desktop-options--company');
+const navConnect = document.querySelector('.desktop-options--connect');
+const navSubProduct = document.querySelector('.desktop-product');
+const navSubCompany = document.querySelector('.desktop-company');
+const navSubConnect = document.querySelector('.desktop-connect');
 const mobileMenu = document.querySelector('.navbar--mobile-menu');
 
 navbarIcon.addEventListener('click', toggleMobileMenu);
@@ -14,14 +14,19 @@ navConnect.addEventListener('click', toggleConnect);
 
 function toggleProduct() {
     navSubProduct.classList.toggle('inactive');
+    navSubCompany.classList.add('inactive');
+    navSubConnect.classList.add('inactive');
 }
 function toggleCompany() {
     navSubCompany.classList.toggle('inactive');
+    navSubProduct.classList.add('inactive');
+    navSubConnect.classList.add('inactive');
 }
 function toggleConnect() {
     navSubConnect.classList.toggle('inactive');
+    navSubProduct.classList.add('inactive');
+    navSubCompany.classList.add('inactive');
 }
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
-    desktopMenu.classList.add('inactive')
 }
