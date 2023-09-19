9fb3c21b74943d3c4ab9147a0d4743a1b2ecf752
S1S4 <Cesar_Zhiro26@hotmail.com>
2023-09-19 15:41:21 -0400

    added active status to mobile-menu

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
 const navbarHamburger = document.querySelector('.icon-hamburger');
 const navbarClose = document.querySelector('.icon-close');
-const navProduct = document.querySelector('.desktop-options--product');
-const navCompany = document.querySelector('.desktop-options--company');
-const navConnect = document.querySelector('.desktop-options--connect');
-const navSubProduct = document.querySelector('.subOption-product');
-const navSubCompany = document.querySelector('.subOption-company');
-const navSubConnect = document.querySelector('.subOption-connect');
+const desktopNavProduct = document.querySelector('.desktop-options--product');
+const desktopNavCompany = document.querySelector('.desktop-options--company');
+const desktopNavConnect = document.querySelector('.desktop-options--connect');
+const desktopNavSubProduct = document.querySelector('.desktop-subOption--product');
+const desktopNavSubCompany = document.querySelector('.desktop-subOption--company');
+const desktopNavSubConnect = document.querySelector('.desktop-subOption--connect');
+const mobileNavProduct = document.querySelector('.mobile-options--product');
+const mobileNavCompany = document.querySelector('.mobile-options--company');
+const mobileNavConnect = document.querySelector('.mobile-options--connect');
+const mobileNavSubProduct = document.querySelector('.mobile-subOption--product');
+const mobileNavSubCompany = document.querySelector('.mobile-subOption--company');
+const mobileNavSubConnect = document.querySelector('.mobile-subOption--connect');
 const mobileMenu = document.querySelector('.navbar--mobile-menu');
 
 navbarHamburger.addEventListener('click', toggleMobileMenu);
 navbarClose.addEventListener('click', closeMobileMenu);
-navProduct.addEventListener('click', toggleProduct);
-navCompany.addEventListener('click', toggleCompany);
-navConnect.addEventListener('click', toggleConnect);
+desktopNavProduct.addEventListener('click', toggleProduct);
+desktopNavCompany.addEventListener('click', toggleCompany);
+desktopNavConnect.addEventListener('click', toggleConnect);
+mobileNavProduct.addEventListener('click', toggleProduct);
+mobileNavCompany.addEventListener('click', toggleCompany);
+mobileNavConnect.addEventListener('click', toggleConnect);
 
 function toggleProduct() {
-    navSubProduct.classList.toggle('inactive');
-    navSubCompany.classList.add('inactive');
-    navSubConnect.classList.add('inactive');
+    desktopNavSubProduct.classList.toggle('inactive');
+    mobileNavSubProduct.classList.toggle('inactive');
+
+    desktopNavSubCompany.classList.add('inactive');
+    mobileNavSubCompany.classList.add('inactive');
+
+    desktopNavSubConnect.classList.add('inactive');
+    mobileNavSubConnect.classList.add('inactive');
 }
 function toggleCompany() {
-    navSubCompany.classList.toggle('inactive');
-    navSubProduct.classList.add('inactive');
-    navSubConnect.classList.add('inactive');
+    desktopNavSubCompany.classList.toggle('inactive');
+    mobileNavSubCompany.classList.toggle('inactive');
+
+    desktopNavSubProduct.classList.add('inactive');
+    mobileNavSubProduct.classList.add('inactive');
+
+    desktopNavSubConnect.classList.add('inactive');
+    mobileNavSubConnect.classList.add('inactive');
 }
 function toggleConnect() {
-    navSubConnect.classList.toggle('inactive');
-    navSubProduct.classList.add('inactive');
-    navSubCompany.classList.add('inactive');
+    desktopNavSubConnect.classList.toggle('inactive');
+    mobileNavSubConnect.classList.toggle('inactive');
+
+    desktopNavSubProduct.classList.add('inactive');
+    mobileNavSubProduct.classList.add('inactive');
+
+    desktopNavSubCompany.classList.add('inactive');
+    mobileNavSubCompany.classList.add('inactive');
 }
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
@@ -40,9 +64,10 @@ function closeMobileMenu() {
     mobileMenu.classList.add('inactive');
 }
 
-// window.addEventListener("click", function(event) {
-//     if (event.target == mobileMenu) {
-//         mobileMenu.style.display = "none";
-//         mobileMenu.classList.add('inactive');
+// let test =  document.getElementById('mobile-menu');
+
+// window.onclick = function(event) {
+//     if (event.target == test) {
+//         test.style.display = "none";
 //     }
-// });
+// };
